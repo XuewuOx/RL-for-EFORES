642fc0987f185fee76a537aab72d000a37d34f41
Daniel Dai <dxw.dai@gmail.com>
2023-01-27 23:29:10 +0000

    adding comments on the arguments of PPO_pipline( )

--- power_distribution_/main.py
+++ power_distribution_/main.py
@@ -31,7 +31,24 @@ def training():
     )
     rl.learn(total_timesteps=10000000)
 
-
+    '''
+       Initilization arguments:
+        :param file_name: The filename where the parameter log file is stored.
+        :param learning_rate: learning rate
+        :param n_steps: The number of timesteps each time the ppo interacts with the environment and samples
+        :param batch_size: batch size for training the model
+        :param n_epochs: number of epochs per training
+        :param gamma: discount factor
+        :param gae_lambda: One of the parameters to calculate GAE in PPO
+        :param clip_range: Parameters for PPO clipping surrogate object update ratio
+        :param clip_range_vf: Parameters for PPO clipping state value object update ratio
+        :param normalize_advantage: whether to normalize estimation advantage value
+        :param ent_coef: param for encouraging exploration
+        :param vf_coef: param for state value object loss proportion
+        :param max_grad_norm: gradient clipping strength
+        :param seed: Number of random seeds
+        :param device: Use 'cpu' or 'gpu' to train the model
+    '''
 if __name__ == '__main__':
 
     training()
